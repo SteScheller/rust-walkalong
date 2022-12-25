2f628304ddcfcf6f743726694ba658091841bbf7
Stefan Scheller <ste.scheller@gmail.com>
2022-12-25 22:11:45 +0100

    added calculation of rectangle area as method

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,8 +1,14 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
 fn main() {
     let width = 30;
     let height = 50;
@@ -16,6 +22,9 @@ fn main() {
         height: 50,
     };
     println!("The area of the rectangle is {} square pixels.", area_struct(&rect));
+    println!("rect is {:?}", rect);
+    println!("rect is {:#?}", rect);
+    println!("The area of the rectangle is {} square pixels.", rect.area());
 }
 
 fn area(width: u32, height: u32) -> u32 {
