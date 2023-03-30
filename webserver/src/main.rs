b7f7b3cd3f42c3bccea8b1b20ce28626bf1711c2
Stefan Scheller <ste.scheller@gmail.com>
2023-03-30 21:21:51 +0200

    continued webserver example

--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -6,7 +6,7 @@ use std::{
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    
+
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
@@ -24,11 +24,23 @@ fn handle_connection(mut stream: TcpStream) {
 
     println!("Request: {:#?}", http_request);
 
-    let status_line = "HTTP/1.1 200 OK";
-    let content = fs::read_to_string("hello.html").unwrap();
-    let length = content.len();
+    let method = &http_request[0];
 
-    let response = format!("{status_line}\r\nContent-length: {length}\r\n\r\n{content}");
+    let response: String;
+    if method == "GET / HTTP/1.1" {
+        let status_line = "HTTP/1.1 200 OK";
+        let content = fs::read_to_string("hello.html").unwrap();
+        let length = content.len();
+
+        response = format!("{status_line}\r\nContent-length: {length}\r\n\r\n{content}");
+    } else {
+        // some other request
+        let status_line = "HTTP/1.1 404 NOT FOUND";
+        let contents = fs::read_to_string("404.html").unwrap();
+        let length = contents.len();
+
+        response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    }
 
     stream.write_all(response.as_bytes()).unwrap();
 }
